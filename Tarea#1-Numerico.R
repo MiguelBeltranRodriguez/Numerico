2c3c46b78affff4c3f6a397963286c03e5a4d52c
Miguel Angel Beltran Rodriguez <41310419+MiguelBeltranRodriguez@users.noreply.github.com>
2018-07-27 08:41:26 -0500

    Update Tarea#1-Numerico.R

--- Tarea#1-Numerico.R
+++ Tarea#1-Numerico.R
@@ -3,15 +3,24 @@
 f<-function(x){
   return(((9.8*68.1)/x)*(1-(exp(1)^(-(x/68.1)*10)))-40);
 }
+
+ferror<-function(ei,ef){
+  
+  return(ef/ei);
+}
+
+
 #biseccion
 biseccion<-function(a,b){
+  
+  
   k = ((a+b)/2);
   plot(f,from=a,to=b)
   abline(h=0,col="blue");
   i = 0;
   while(abs(f(k))>=(1.e-8)){
     
-    
+    i = i +1;
     if(f(a)*f(k)<0)
     {
       b=k;
@@ -24,8 +33,10 @@ biseccion<-function(a,b){
     
     k <- ((a+b)/2);
     cat("i=",i,"\tX=",k,"\n");
-    i=i+1;
+    
   }
+  
+ 
   return (k);
   
 }
@@ -33,21 +44,30 @@ biseccion<-function(a,b){
 
 triseccion<-function(a,b,c){
   
+  
+  e = 0;
+  
   k = ((a+b+c)/3);
   plot(f,from=a,to=c)
   abline(h=0,col="blue");
   i = 0;
   while(abs(f(k))>=(1.e-8)){
     
-    i = i +1;
+    print(ferror(e,abs( abs(f(k))-e))) ; 
+    
+    e =abs( abs(f(k))-e); 
+    
+   
+    
+    
     if(f(a)*f(k)<0)
     {
-        if(f(b)*f(k)<0)
-        {
-          a=b;
-          b=(b+k)/2;
-          c=k;
-        }
+      if(f(b)*f(k)<0)
+      {
+        a=b;
+        b=(b+k)/2;
+        c=k;
+      }
       else
       {
         b=(a+k)/2;
@@ -70,19 +90,22 @@ triseccion<-function(a,b,c){
       }
     }
     
-    text(k,0,i,cex=0.8,col="red")
+    text(k,0,i,cex=0.7,col="red")
     
-      k <- ((a+b+c)/3);
-      cat("i=",i,"\tX=",k,"\n");
-      i=i+1;
+    k <- ((a+b+c)/3);
+    cat("i=",i,"\tX=",k,"\n");
+    i=i+1;
+    
   }
+  
+  
   return (k);
 }
 
 #tetra
 
 tetraseccion<-function(a,b,c,d){
-
+  
   k = ((a+b+c+d)/4);
   plot(f,from=a,to=d)
   abline(h=0,col="blue");
@@ -91,22 +114,22 @@ tetraseccion<-function(a,b,c,d){
   k = biseccion(a,b);
   if(abs(f(k))>=(1.e-8)){
     k = biseccion(b,c);
-      if(abs(f(k))>=(1.e-8)){
-        k = biseccion(c,d);
-      }
+    if(abs(f(k))>=(1.e-8)){
+      k = biseccion(c,d);
     }
-    
-    
-  text(k,0,i,cex=0.8,col="red")
+  }
+  
+  
+  text(k,0,i,cex=0.5,col="red")
   cat("i=",i,"\tX=",k,"\n");
-   
+  
   
   return (k);
 }
 
 #pruebas
-coeficiente = biseccion(1,100)
+coeficiente = biseccion(14,16)
 coeficiente
-coeficiente = triseccion(1,50,100)
+coeficiente = triseccion(-1000000,1,1000000)
 coeficiente
 f(coeficiente)
